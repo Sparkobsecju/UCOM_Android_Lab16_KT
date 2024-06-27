ce24afb296255d23cc15bffc0a7e9c40fb6b8bc1
Sivan Chen <ryan1825114@gmail.com>
2024-06-27 15:50:43 +0800

    RandomService

--- app/src/main/java/com/example/lab16_kt/MainActivity.kt
+++ app/src/main/java/com/example/lab16_kt/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.lab16_kt
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.os.RemoteException
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -8,7 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ServiceConnection {
     private lateinit var button1: Button
     private lateinit var textView1: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +27,32 @@ class MainActivity : AppCompatActivity() {
         }
         button1 = findViewById(R.id.button1)
         textView1 = findViewById(R.id.textView1)
+        val serviceIntent = Intent(this, RandomService::class.java)
+        bindService(serviceIntent, this, BIND_AUTO_CREATE)
+        button1.setOnClickListener { getRandomFromService() }
+    }
+
+    private fun getRandomFromService() {
+        try {
+
+            val result = randomService!!.getRandom(System.currentTimeMillis())
+            textView1.text = "取得的結果是${result}"
+        } catch (e: RemoteException) {
+            textView1.text = "oops, 錯誤是${e.message.toString()}"
+        }
+    }
+
+    override fun onDestroy() {
+        unbindService(this)
+        super.onDestroy()
+    }
+
+    var randomService: IRandomService? = null
+    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+        randomService = IRandomService.Stub.asInterface(service)
+    }
+
+    override fun onServiceDisconnected(name: ComponentName?) {
+        randomService = null
     }
 }
